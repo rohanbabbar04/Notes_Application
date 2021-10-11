b9e4cab59e9631afe0e6a4546287f3b23a8655fa
rohanbabbar <rohanbabbar0408@gmail.com>
2021-10-12 00:48:45 +0530

    Added NoteDAO

---
+++ app/src/main/java/com/example/notes_application/NoteDAO.java
@@ -0,0 +1,27 @@
+package com.example.notes_application;
+
+import androidx.lifecycle.LiveData;
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import java.util.List;
+
+@Dao
+public interface NoteDAO {
+
+    @Insert
+    void Insert(Note note);
+
+    @Update
+    void update(Note note);
+
+    @Delete
+    void delete(Note note);
+
+    @Query("SELECT * FROM note_table ORDER BY id ASC")
+    LiveData<List<Note>> getAllNotes();
+
+}
