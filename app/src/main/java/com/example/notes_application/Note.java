455dcdf1caa9e7d7a8eb6305c7c6b1f807cf8fb7
rohanbabbar <rohanbabbar0408@gmail.com>
2021-10-12 00:43:22 +0530

    Added Note Entity and build for Room DB

---
+++ app/src/main/java/com/example/notes_application/Note.java
@@ -0,0 +1,37 @@
+package com.example.notes_application;
+
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "note_table")
+public class Note {
+
+    @PrimaryKey(autoGenerate = true)
+    private int id;
+
+    private final String title;
+
+    private final String description;
+
+    public Note(String title, String description) {
+        this.title = title;
+        this.description = description;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
+
