14724a7a583211d5542dde827589dd716f75ad06
rohanbabbar <rohanbabbar0408@gmail.com>
2021-10-12 18:32:03 +0530

    Added Title

--- app/src/main/java/com/example/notes_application/MainActivity.java
+++ app/src/main/java/com/example/notes_application/MainActivity.java
@@ -35,6 +35,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Objects.requireNonNull(getSupportActionBar()).setTitle("Make Notes");
         recyclerView = findViewById(R.id.recyclerView);
         recyclerView.setLayoutManager(new LinearLayoutManager(MainActivity.this));
         adapter = new RecyclerViewAdapter(MainActivity.this);
