552a2f215fa6e95d2f65b63a5f7eabfe95529d92
rohanbabbar <rohanbabbar0408@gmail.com>
2021-10-12 15:36:28 +0530

    Added MainActivity

--- app/src/main/java/com/example/notes_application/MainActivity.java
+++ app/src/main/java/com/example/notes_application/MainActivity.java
@@ -1,14 +1,106 @@
 package com.example.notes_application;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.ItemTouchHelper;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
 
+    private RecyclerView recyclerView;
+    private NoteViewModel noteViewModel;
+    private RecyclerViewAdapter adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
+        recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(MainActivity.this));
+        adapter = new RecyclerViewAdapter(MainActivity.this);
+        recyclerView.setAdapter(adapter);
+        noteViewModel = new ViewModelProvider.AndroidViewModelFactory(getApplication()).create(NoteViewModel.class);
+        noteViewModel.getAllNotes().observe(this, new Observer<List<Note>>() {
+            @Override
+            public void onChanged(List<Note> notes) {
+                // update RecyclerView
+                adapter.setNotes(notes);
+            }
+        });
+
+        new ItemTouchHelper(new ItemTouchHelper.SimpleCallback(0,ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT) {
+            @Override
+            public boolean onMove(@NonNull @NotNull RecyclerView recyclerView, @NonNull @NotNull RecyclerView.ViewHolder viewHolder, @NonNull @NotNull RecyclerView.ViewHolder target) {
+                return false;
+            }
+
+            @Override
+            public void onSwiped(@NonNull @NotNull RecyclerView.ViewHolder viewHolder, int direction) {
+                noteViewModel.delete(adapter.getNotes(viewHolder.getAdapterPosition()));
+            }
+        }).attachToRecyclerView(recyclerView);
+
+        adapter.setOnItemClickListener(new RecyclerViewAdapter.onItemClickListener() {
+            @Override
+            public void onItemClick(Note note) {
+                Intent intent = new Intent(MainActivity.this,UpdateNoteActivity.class);
+                intent.putExtra("id",note.getId());
+                intent.putExtra("title",note.getTitle());
+                intent.putExtra("description",note.getDescription());
+                startActivityForResult(intent,2);
+            }
+        });
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.new_menu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.top_menu:
+                Intent intent = new Intent(MainActivity.this,AddNoteActivity.class);
+                startActivityForResult(intent,1);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable @org.jetbrains.annotations.Nullable Intent data) {
+        super.onActivityResult(requestCode,resultCode,data);
+        if (requestCode == 1 && resultCode == RESULT_OK) {
+            String noteTitle = data.getStringExtra("noteTitle");
+            String noteDescription = data.getStringExtra("noteDescription");
+            Note note = new Note(noteTitle,noteDescription);
+            noteViewModel.insert(note);
+        }else if (requestCode == 2 && resultCode == RESULT_OK) {
+            String noteTitle = data.getStringExtra("lastTitle");
+            String noteDescription = data.getStringExtra("lastDescription");
+            int id = data.getIntExtra("lastId",-1);
+            Note note = new Note(noteTitle,noteDescription);
+            note.setId(id);
+            noteViewModel.update(note);
+        }
     }
 }
